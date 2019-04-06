5235ee0a95e6d27694a0934b94f0dbd78a02aedf
vinay <vinu.db@gmail.com>
2019-04-06 22:07:21 +0530

    commented all firebase files

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import { firebase } from './firebase/firebase';
+//import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
+import DashboardPage from './components/DashboardPage';
 
 const store = configureStore();
 const jsx = (
@@ -24,18 +25,18 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<LoadingPage />, document.getElementById('app'));
+ReactDOM.render(<DashboardPage />, document.getElementById('app'));
 
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    store.dispatch(login(user.uid));
-    renderApp();
-    if (history.location.pathname === '/') {
-      history.push('/dashboard');
-    }
-  } else {
-    store.dispatch(logout());
-    renderApp();
-    history.push('/');
-  }
-});
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (user) {
+//     store.dispatch(login(user.uid));
+//     renderApp();
+//     if (history.location.pathname === '/') {
+//       history.push('/dashboard');
+//     }
+//   } else {
+//     store.dispatch(logout());
+//     renderApp();
+//     history.push('/');
+//   }
+// });
